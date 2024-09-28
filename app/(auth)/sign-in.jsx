c0d846b544f26c23de5dadadaba3ec45ef7f339a
Gaor-jjj <kaurtamm1995@gmail.com>
2024-09-28 16:53:22 +0300

    Sign in and Login logic

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,22 +1,44 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { useGlobalContext } from '../../context/GlobalProvider';
+
+import { getCurrentUser, signIn } from '../../lib/appwrite';
 
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const [form, setForm] = useState({
     email: '',
     password: ''
   })
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const submit = async () => {
+    if(form.email === "" || form.password === "") {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
 
-  const submit = () => {
+    setIsSubmitting(true);
 
+    try {
+      await signIn(form.email, form.password)
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLoggedIn(true);
+
+      Alert.alert('Success', 'User signed in successfully');
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,23 +1,42 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { createUser } from '../../lib/appwrite';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 const SignUp = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const [form, setForm] = useState({
     username: '',
     email: '',
     password: ''
   })
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const submit = async () => {
+    if(form.username === "" || form.email === "" || form.passwor === "") {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
 
-  const submit = () => {
+    setIsSubmitting(true);
 
+    try {
+      const result = await createUser(form.email, form.password, form.username)
+      setUser(result);
+      setIsLoggedIn(true);
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
@@ -48,7 +67,7 @@ const SignUp = () => {
             otherStyles="mt-7"
           />
           <CustomButton
-            title="Sing In"
+            title="Sing Up"
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
